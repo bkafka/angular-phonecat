aa6081d97f439d762edcc8e516c6ad200cb94543
bkafka <bendakafka@gmail.com>
2015-06-12 08:35:14 -0400

    initial tutorial files I created

---
+++ app/js/bensfunctions.js
@@ -0,0 +1,17 @@
+/**
+ *  assert
+ */
+
+ function assert (value,desc ) {
+    var results;
+    var li = document.createElement("li");
+    li.className= value ? "pass":"fail";
+    li.appendChild(document.createTextNode(desc));
+    document.getElementById("results").appendChild(li);
+
+}
+
+
+
+
+
---
+++ app/js/bensfunctions2.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ben on 2/23/14.
+ */
+assert(true,"bensfunctions2");
